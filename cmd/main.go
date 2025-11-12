c3ec8e72d9e7c9fef23684d7e992cecca5435243
freekieb7 <freekieb7@hotmail.com>
2025-11-12 21:58:57 +0100

    init test

--- cmd/main.go
+++ cmd/main.go
@@ -31,7 +31,7 @@ func serve(ctx context.Context) error {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, World! ")
 	})
 
 	if err := app.Listen(":3000"); err != nil {
